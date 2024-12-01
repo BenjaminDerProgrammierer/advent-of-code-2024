fe3cf241ead9dc47b0ddbf6d18f6afafb886f4f1
Benjamin Hartmann <benjaminhartmann@duck.com>
2024-12-01 13:46:28 +0100

    Initial commit

---
+++ AdventOfCode2024.cs
@@ -0,0 +1,35 @@
+﻿namespace advent_of_code_2024
+{
+    static class AdventOfCode2024
+    {
+        public static readonly string BASE_PATH = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../");
+        public static void Main(string[] args)
+        {
+            Day01.Solve(BASE_PATH + "days/day01/input.txt");
+            // Day02.Solve(BASE_PATH + "days/day02/input.txt");
+            // Day03.Solve(BASE_PATH + "days/day03/input.txt");
+            // Day04.Solve(BASE_PATH + "days/day04/input.txt");
+            // Day05.Solve(BASE_PATH + "days/day05/input.txt");
+            // Day06.Solve(BASE_PATH + "days/day06/input.txt");
+            // Day07.Solve(BASE_PATH + "days/day07/input.txt");
+            // Day08.Solve(BASE_PATH + "days/day08/input.txt");
+            // Day09.Solve(BASE_PATH + "days/day09/input.txt");
+            // Day10.Solve(BASE_PATH + "days/day10/input.txt");
+            // Day11.Solve(BASE_PATH + "days/day11/input.txt");
+            // Day12.Solve(BASE_PATH + "days/day12/input.txt");
+            // Day13.Solve(BASE_PATH + "days/day13/input.txt");
+            // Day14.Solve(BASE_PATH + "days/day14/input.txt");
+            // Day15.Solve(BASE_PATH + "days/day15/input.txt");
+            // Day16.Solve(BASE_PATH + "days/day16/input.txt");
+            // Day17.Solve(BASE_PATH + "days/day17/input.txt");
+            // Day18.Solve(BASE_PATH + "days/day18/input.txt");
+            // Day19.Solve(BASE_PATH + "days/day19/input.txt");
+            // Day20.Solve(BASE_PATH + "days/day20/input.txt");
+            // Day21.Solve(BASE_PATH + "days/day21/input.txt");
+            // Day22.Solve(BASE_PATH + "days/day22/input.txt");
+            // Day23.Solve(BASE_PATH + "days/day23/input.txt");
+            // Day24.Solve(BASE_PATH + "days/day24/input.txt");
+            // Day25.Solve(BASE_PATH + "days/day25/input.txt");
+        }
+    }
+}
