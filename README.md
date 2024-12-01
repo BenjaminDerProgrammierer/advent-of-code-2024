bdaea6e6d30bf44cb0676509b27b2aa21d4c3659
Benjamin Hartmann <benjaminhartmann@duck.com>
2024-12-01 19:11:37 +0100

    Update README

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 ![Static Badge](https://img.shields.io/badge/BUILT_WITH-%E2%9D%A4-d15d26?style=for-the-badge&labelColor=e36d25)
 ![Static Badge](https://img.shields.io/badge/MADE_WITH-C%23-c0282d?style=for-the-badge&labelColor=ef4041)
 ![Static Badge](https://img.shields.io/badge/I_USE-VISUAL_STUDIO_CODE-0078d4?style=for-the-badge&labelColor=004d87)
-
 ![Static Badge](https://img.shields.io/badge/IT_WORKS-NO_IDEO_WHY-ff6348?style=for-the-badge&labelColor=3bb371)
 
 ---
---
+++ days/day02/Day02.cs
@@ -0,0 +1,23 @@
+namespace advent_of_code_2024
+{
+    public static class Day02
+    {
+        public static void Solve(string inputPath)
+        {
+            Console.WriteLine($"DAY 02 | Part 1: {Day01.Part1(File.ReadAllText(inputPath))}, Part 2: {Day01.Part2(File.ReadAllText(inputPath))}");
+        }
+        public static int Part1(string input)
+        {
+            int result = 0;
+            // Your code here
+            return result;
+        }
+
+        public static int Part2(string input)
+        {
+            int result = 0;
+            // Your code here
+            return result;
+        }
+    }
+}
