cbd8ed5d0c6079d892528d746f27de9d77692685
Benjamin Hartmann <benjaminhartmann@duck.com>
2024-12-02 21:05:41 +0100

    Day 2

--- AdventOfCode2024.cs
+++ AdventOfCode2024.cs
@@ -6,7 +6,7 @@
         public static void Main(string[] args)
         {
             Day01.Solve(BASE_PATH + "days/day01/input.txt");
-            // Day02.Solve(BASE_PATH + "days/day02/input.txt");
+            Day02.Solve(BASE_PATH + "days/day02/input.txt");
             // Day03.Solve(BASE_PATH + "days/day03/input.txt");
             // Day04.Solve(BASE_PATH + "days/day04/input.txt");
             // Day05.Solve(BASE_PATH + "days/day05/input.txt");
--- days/day02/Day02.cs
+++ days/day02/Day02.cs
@@ -10,159 +10,84 @@ namespace advent_of_code_2024
         }
         public static int Part1(string input)
         {
-            int errors = 0;
+            int safes = 0;
             int[][] reports = input.Split("\n")
                                 .Select(line => line.Split(" ")
                                                     .Select(int.Parse)
                                                     .ToArray())
                                 .ToArray();
-            bool error = false;
             foreach (int[] level in reports)
             {
-                if (level[0] > level[1])
+                if (isSafe(level))
                 {
-                    // decreasing
-                    for (int i = 1; i < level.Length; i++)
-                    {
-                        if (level[i] > level[i - 1]) // suddenly increasing
-                        {
-                            error = true;
-                            break;
-                        }
-                        if (Math.Abs(level[i - 1] - level[i]) > 3 || Math.Abs(level[i - 1] - level[i]) == 0) // Any two adjacent levels are only allowed to differ by at least one and at most three.
-                        {
-                            error = true;
-                            break;
-                        }
-                    }
-                }
-                else
-                {
-                    // increasing
-                    for (int i = 1; i < level.Length; i++)
-                    {
-                        if (level[i] < level[i - 1]) // suddenly decreasing
-                        {
-                            error = true;
-                            break;
-                        }
-                        if (Math.Abs(level[i - 1] - level[i]) > 3 || Math.Abs(level[i - 1] - level[i]) == 0) // Any two adjacent levels are only allowed to differ by at least one and at most three.
-                        {
-                            error = true;
-                            break;
-                        }
-                    }
-                }
-                if (error)
-                {
-                    error = false;
-                    errors++;
+                    safes++;
                 }
             }
 
-            return 1000 - errors;
+            return safes;
         }
 
         public static int Part2(string input)
         {
-            // //Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
-            // int errors = 0;
-            // int[][] reports = input.Split("\n")
-            //                     .Select(line => line.Split(" ")
-            //                                         .Select(int.Parse)
-            //                                         .ToArray())
-            //                     .ToArray();
-            // bool error = false;
-            // for (int i = 0; i < reports.Length; i++)
-            // {
-            //     int[] level = reports[i];
-            //     if (level[0] > level[1])
-            //     {
-            //         // decreasing
-            //         for (int j = 1; j < level.Length; j++)
-            //         {
-            //             if (level[j] > level[j - 1]) // suddenly increasing
-            //             {
-            //                 error = true;
-            //                 break;
-            //             }
-            //             if (Math.Abs(level[j - 1] - level[j]) > 3 || Math.Abs(level[j - 1] - level[j]) == 0) // Any two adjacent levels are only allowed to differ by at least one and at most three.
-            //             {
-            //                 error = true;
-            //                 break;
-            //             }
-            //         }
-            //     }
-            //     else
-            //     {
-            //         // increasing
-            //         for (int j = 1; j < level.Length; j++)
-            //         {
-            //             if (level[j] < level[j - 1]) // suddenly decreasing
-            //             {
-            //                 error = true;
-            //                 break;
-            //             }
-            //             if (Math.Abs(level[j - 1] - level[j]) > 3 || Math.Abs(level[j - 1] - level[j]) == 0) // Any two adjacent levels are only allowed to differ by at least one and at most three.
-            //             {
-            //                 error = true;
-            //                 break;
-            //             }
-            //         }
-            //     }
-            //     if (error)
-            //     {
-            //         error = false;
-            //         // try removing one level
-            //         for (int j = 0; j < level.Length; j++)
-            //         {
-            //             int[] newLevel = level.Where((source, index) => index != j).ToArray();
-            //             if (newLevel[0] > newLevel[1])
-            //             {
-            //                 // decreasing
-            //                 for (int k = 1; k < newLevel.Length; k++)
-            //                 {
-            //                     if (newLevel[k] > newLevel[k - 1]) // suddenly increasing
-            //                     {
-            //                         error = true;
-            //                         break;
-            //                     }
-            //                     if (Math.Abs(newLevel[k - 1] - newLevel[k]) > 3 || Math.Abs(newLevel[k - 1] - newLevel[k]) == 0) // Any two adjacent levels are only allowed to differ by at least one and at most three.
-            //                     {
-            //                         error = true;
-            //                         break;
-            //                     }
-            //                 }
-            //             }
-            //             else
-            //             {
-            //                 // increasing
-            //                 for (int k = 1; k < newLevel.Length; k++)
-            //                 {
-            //                     if (newLevel[k] < newLevel[k - 1]) // suddenly decreasing
-            //                     {
-            //                         error = true;
-            //                         break;
-            //                     }
-            //                     if (Math.Abs(newLevel[k - 1] - newLevel[k]) > 3 || Math.Abs(newLevel[k - 1] - newLevel[k]) == 0) // Any two adjacent levels are only allowed to differ by at least one and at most three.
-            //                     {
-            //                         error = true;
-            //                         break;
-            //                     }
-            //                 }
-            //             }
-            //             if (!error)
-            //             {
-            //                 reports[i] = newLevel;
-            //                 break;
-            //             }
-            //             error = false;
-            //         }
-            //         errors++;
-            //     }
-            // }
-            // return 1000 - errors;
-            return 0;
+            int safes = 0;
+            int[][] reports = input.Split("\n")
+                                .Select(line => line.Split(" ")
+                                                    .Select(int.Parse)
+                                                    .ToArray())
+                                .ToArray();
+            foreach (int[] level in reports)
+            {
+                for (int i = -1; i < level.Length; i++)
+                {
+                    int[] newLevel = level;
+                    // remove element i from the array
+                    if (i != -1)
+                    {
+                        newLevel = level[..i].Concat(level[(i + 1)..]).ToArray();
+                    }
+                    if (isSafe(newLevel))
+                    {
+                        safes++;
+                        break;
+                    }
+                }
+            }
+            return safes;
+        }
+
+        static private bool isSafe(int[] level)
+        {
+            if (level[0] > level[1])
+            {
+                // decreasing
+                for (int i = 1; i < level.Length; i++)
+                {
+                    if (level[i] > level[i - 1]) // suddenly increasing
+                    {
+                        return false;
+                    }
+                    if (Math.Abs(level[i - 1] - level[i]) > 3 || Math.Abs(level[i - 1] - level[i]) == 0) // Any two adjacent levels are only allowed to differ by at least one and at most three.
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                // increasing
+                for (int i = 1; i < level.Length; i++)
+                {
+                    if (level[i] < level[i - 1]) // suddenly decreasing
+                    {
+                        return false;
+                    }
+                    if (Math.Abs(level[i - 1] - level[i]) > 3 || Math.Abs(level[i - 1] - level[i]) == 0) // Any two adjacent levels are only allowed to differ by at least one and at most three.
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
     }
 }
