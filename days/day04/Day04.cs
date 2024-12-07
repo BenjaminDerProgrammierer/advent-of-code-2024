562e90dbe55c01bfc6d6e244ae2345b7b5ee8905
Benjamin Hartmann <benjaminhartmann@duck.com>
2024-12-07 21:22:02 +0100

    Day 4

--- days/day04/Day04.cs
+++ days/day04/Day04.cs
@@ -13,117 +13,108 @@ namespace advent_of_code_2024
         public static int Part1(string input)
         {
             int result = 0;
-            string pattern = "X";
             char[][] arr = input
                 .Split('\n') // Split the string by new lines
                 .Select(line => line.ToCharArray()) // Convert each line to a char array
                 .ToArray(); // Convert the IEnumerable<char[]> to a char[][]
 
-
-            foreach (var row in arr)
+            // look for X
+            for (int y = 0; y < arr.Length; y++)
             {
-                Console.WriteLine(string.Join("", row));
+                char[] row = arr[y];
+                for (int x = 0; x < row.Length; x++)
+                {
+                    char c = row[x];
+                    if (c == 'X')
+                    {
+                        Console.WriteLine($"Found 'X' at ({y}, {x})");
+
+                        // search for Ms
+                        IEnumerable<int[]> mMatches = GetSurroundingChars(x, y, arr, 'M');
+                        foreach (var match in mMatches)
+                        {
+                            // search for A
+                            int[]? direction = CheckDirection(x + match[0] * 2, y + match[1] * 2, match[0], match[1], arr, 'A');
+                            if (direction != null)
+                            {
+                                // search for S
+                                direction = CheckDirection(x + match[0] * 3, y + match[1] * 3, match[0], match[1], arr, 'S');
+                                if (direction != null)
+                                {
+                                    result++;
+                                }
+                            }
+                        }
+                    }
+                }
             }
 
-            foreach (Match m in Regex.Matches(input, pattern))
+            Console.WriteLine("------");
+            return result;
+        }
+
+        public static int Part2(string input)
+        {
+            int result = 0;
+            char[][] arr = input
+                .Split('\n') // Split the string by new lines
+                .Select(line => line.ToCharArray()) // Convert each line to a char array
+                .ToArray(); // Convert the IEnumerable<char[]> to a char[][]
+                            
+            // You misunderstood the task, you should not find XMAS in any direction, you soulf find an x out of MAS
+            // M.S
+            // .A.
+            // M.S
+
+            // M.M
+            // .A.
+            // s.S
+
+            // S.S
+            // .A.
+            // M.M
+
+            // M.S
+            // .A.
+            // M.S
+
+            // look for A
+            for (int y = 0; y < arr.Length; y++)
             {
-                Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
-                //search for any (2d) surrounding Ms
-                List<int[]> directions = new List<int[]>();
-                int row = m.Index / arr.Length;
-                int col = m.Index % arr.Length;
+                char[] row = arr[y];
+                for (int x = 0; x < row.Length; x++)
+                {
+                    char c = row[x];
+                    if (c == 'A')
+                    {
+                        Console.WriteLine($"Found 'A' at ({y}, {x})");
 
-                if (row != 0)
-                {
-                    //top
-                    if (arr[row - 1][col] == 'M')
-                    {
-                        directions.Add(new int[] { 0, -1 });
-                        Console.WriteLine($"Found 'M' at (-1, 0)");
-                    }
-                    if (col != 0)
-                    {
-                        //top left
-                        if (arr[row - 1][col - 1] == 'M')
+                        // search for the pattern
+                        if (
+                            (
+                                CheckDirection(x - 1, y - 1, -1, -1, arr, 'M') != null && // top-left
+                                CheckDirection(x + 1, y - 1, 1, -1, arr, 'S') != null && // top-right
+                                CheckDirection(x - 1, y + 1, -1, 1, arr, 'M') != null && // bottom-left
+                                CheckDirection(x + 1, y + 1, 1, 1, arr, 'S') != null // bottom-right
+                            ) || (
+                                CheckDirection(x - 1, y - 1, -1, -1, arr, 'M') != null && // top-left
+                                CheckDirection(x + 1, y - 1, 1, -1, arr, 'M') != null && // top-right
+                                CheckDirection(x - 1, y + 1, -1, 1, arr, 'S') != null && // bottom-left
+                                CheckDirection(x + 1, y + 1, 1, 1, arr, 'S') != null // bottom-right
+                            ) || (
+                                CheckDirection(x - 1, y - 1, -1, -1, arr, 'S') != null && // top-left
+                                CheckDirection(x + 1, y - 1, 1, -1, arr, 'S') != null && // top-right
+                                CheckDirection(x - 1, y + 1, -1, 1, arr, 'M') != null && // bottom-left
+                                CheckDirection(x + 1, y + 1, 1, 1, arr, 'M') != null // bottom-right
+                            ) || (
+                                CheckDirection(x - 1, y - 1, -1, -1, arr, 'S') != null && // top-left
+                                CheckDirection(x + 1, y - 1, 1, -1, arr, 'M') != null && // top-right
+                                CheckDirection(x - 1, y + 1, -1, 1, arr, 'S') != null && // bottom-left
+                                CheckDirection(x + 1, y + 1, 1, 1, arr, 'M') != null // bottom-right
+                            )
+                        )
                         {
-                            directions.Add(new int[] { -1, -1 });
-                            Console.WriteLine($"Found 'M' at (-1, -1)");
-                        }
-                    }
-                    if (col != arr.Length - 1)
-                    {
-                        //top right
-                        if (arr[row - 1][col + 1] == 'M')
-                        {
-                            directions.Add(new int[] { 1, -1 });
-                            Console.WriteLine($"Found 'M' at (1, -1)");
-                        }
-                    }
-                }
-                if (row != arr.Length - 1)
-                {
-                    //bottom
-                    if (arr[row + 1][col] == 'M')
-                    {
-                        directions.Add(new int[] { 0, 1 });
-                        Console.WriteLine($"Found 'M' at (0, 1)");
-                    }
-                    if (col != 0)
-                    {
-                        //bottom left
-                        if (arr[row + 1][col - 1] == 'M')
-                        {
-                            directions.Add(new int[] { -1, 1 });
-                            Console.WriteLine($"Found 'M' at (-1, 1)");
-                        }
-                    }
-                    if (col != arr.Length - 1)
-                    {
-                        //bottom right
-                        if (arr[row + 1][col + 1] == 'M')
-                        {
-                            directions.Add(new int[] { 1, 1 });
-                            Console.WriteLine($"Found 'M' at (1, 1)");
-                        }
-                    }
-                }
-                if (col != 0)
-                {
-                    //left
-                    if (arr[row][col - 1] == 'M')
-                    {
-                        directions.Add(new int[] { -1, 0 });
-                        Console.WriteLine($"Found 'M' at (0, -1)");
-                    }
-                }
-                if (col != arr.Length - 1)
-                {
-                    //right
-                    if (arr[row][col + 1] == 'M')
-                    {
-                        directions.Add(new int[] { 1, 0 });
-                        Console.WriteLine($"Found 'M' at (0, +1)");
-                    }
-                }
-                //if there are any Ms surrounding the X
-                if (directions.Count > 0)
-                {
-                    //check if there are any As in the same directions
-                    foreach (var direction in directions)
-                    {
-                        int newRowA = row + direction[0] * 2;
-                        int newColA = col + direction[1] * 2;
-                        int newRowS = row + direction[0] * 3;
-                        int newColS = col + direction[1] * 3;
-
-                        if (newRowA >= 0 && newRowA < arr.Length && newColA >= 0 && newColA < arr[0].Length && arr[newRowA][newColA] == 'A')
-                        {
-                            Console.WriteLine($"Found 'A'");
-                            if (newRowS >= 0 && newRowS < arr.Length && newColS >= 0 && newColS < arr[0].Length && arr[newRowS][newColS] == 'S')
-                            {
-                                Console.WriteLine($"Found 'S'");
-                                result++;
-                            }
+                            result++;
                         }
                     }
                 }
@@ -131,11 +122,46 @@ namespace advent_of_code_2024
             return result;
         }
 
-        public static int Part2(string input)
+        private static List<int[]> GetSurroundingChars(int x, int y, char[][] arr, char searchChar)
         {
-            int result = 0;
-            // your code here
-            return result;
+            List<int[]> directions = new List<int[]>();
+
+            // Check all 8 directions, if the character is found (not null), add the direction to the list
+            var direction = CheckDirection(x, y - 1, 0, -1, arr, searchChar);  // top
+            if (direction != null) directions.Add(direction);
+            direction = CheckDirection(x - 1, y - 1, -1, -1, arr, searchChar); // top-left
+            if (direction != null) directions.Add(direction);
+            direction = CheckDirection(x + 1, y - 1, 1, -1, arr, searchChar);  // top-right
+            if (direction != null) directions.Add(direction);
+            direction = CheckDirection(x, y + 1, 0, 1, arr, searchChar);       // bottom
+            if (direction != null) directions.Add(direction);
+            direction = CheckDirection(x - 1, y + 1, -1, 1, arr, searchChar);  // bottom-left
+            if (direction != null) directions.Add(direction);
+            direction = CheckDirection(x + 1, y + 1, 1, 1, arr, searchChar);   // bottom-right
+            if (direction != null) directions.Add(direction);
+            direction = CheckDirection(x - 1, y, -1, 0, arr, searchChar);      // left
+            if (direction != null) directions.Add(direction);
+            direction = CheckDirection(x + 1, y, 1, 0, arr, searchChar);       // right
+            if (direction != null) directions.Add(direction);
+
+            return directions;
+        }
+
+        private static int[]? CheckDirection(int x, int y, int dx, int dy, char[][] arr, char searchChar)
+        {
+            try
+            {
+                if (arr[y][x] == searchChar)
+                {
+                    Console.WriteLine($" Found '{searchChar}' at ({x + dx}, {y + dy})");
+                    return new int[] { dx, dy };
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return null;
         }
     }
 }
